08533df824f27d0849cd58e6f23b5ba68376c391
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-28 18:51:17 +0100

    Refactor entry and entryDetail API handlers to improve method handling and streamline response logic

--- src/pages/api/entry.ts
+++ src/pages/api/entry.ts
@@ -5,13 +5,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    return createEntry(req, res);
-  }
   if (req.method === "GET") {
     return getEntries(req, res);
+  } else if (req.method === "POST") {
+    return createEntry(req, res);
+  } else {
+    res.status(405).json({ error: "Metodo non consentito" });
   }
-  res.status(405).json({ error: "Metodo non consentito" });
+}
+
+export async function getEntries(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    const entries = await prisma.entry.findMany();
+    res.status(200).json(entries);
+  } catch (error) {
+    res.status(500).json({ error: "Errore nel recupero delle Entries" });
+  }
 }
 
 export async function createEntry(req: NextApiRequest, res: NextApiResponse) {
@@ -27,14 +36,5 @@ export async function createEntry(req: NextApiRequest, res: NextApiResponse) {
     res.status(201).json(entry);
   } catch (error) {
     res.status(500).json({ error: "Errore nella creazione dell'Entry" });
-  }
-}
-
-export async function getEntries(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    const entries = await prisma.entry.findMany();
-    res.status(200).json(entries);
-  } catch (error) {
-    res.status(500).json({ error: "Errore nel recupero delle Entries" });
   }
 }
--- src/pages/api/entryDetail.ts
+++ src/pages/api/entryDetail.ts
@@ -5,40 +5,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    return createEntryDetail(req, res);
-  }
   if (req.method === "GET") {
     return getEntryDetails(req, res);
-  }
-  res.status(405).json({ error: "Metodo non consentito" });
-}
-
-export async function createEntryDetail(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  if (req.method === "POST") {
-    try {
-      const { user, country, ip, device, isDangerous, tags } = req.body;
-      const entryDetail = await prisma.entryDetail.create({
-        data: {
-          user,
-          country,
-          ip,
-          device,
-          isDangerous,
-          tags: {
-            create: tags,
-          },
-        },
-      });
-      res.status(201).json(entryDetail);
-    } catch (error) {
-      res
-        .status(500)
-        .json({ error: "Errore nella creazione dell'EntryDetail" });
-    }
+  } else if (req.method === "POST") {
+    return createEntryDetail(req, res);
   } else {
     res.status(405).json({ error: "Metodo non consentito" });
   }
@@ -48,24 +18,44 @@ export async function getEntryDetails(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "GET") {
-    try {
-      const entryDetails = await prisma.entryDetail.findMany({
-        include: {
-          tags: {
-            select: {
-              title: true,
-              description: true,
-              color: true,
-            },
+  try {
+    const entryDetails = await prisma.entryDetail.findMany({
+      include: {
+        tags: {
+          select: {
+            title: true,
+            description: true,
+            color: true,
           },
         },
-      });
-      res.status(200).json(entryDetails);
-    } catch (error) {
-      res.status(500).json({ error: "Errore nel recupero degli EntryDetails" });
-    }
-  } else {
-    res.status(405).json({ error: "Metodo non consentito" });
+      },
+    });
+    res.status(200).json(entryDetails);
+  } catch (error) {
+    res.status(500).json({ error: "Errore nel recupero degli EntryDetails" });
+  }
+}
+
+export async function createEntryDetail(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { user, country, ip, device, isDangerous, tags } = req.body;
+    const entryDetail = await prisma.entryDetail.create({
+      data: {
+        user,
+        country,
+        ip,
+        device,
+        isDangerous,
+        tags: {
+          create: tags,
+        },
+      },
+    });
+    res.status(201).json(entryDetail);
+  } catch (error) {
+    res.status(500).json({ error: "Errore nella creazione dell'EntryDetail" });
   }
 }
