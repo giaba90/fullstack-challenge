91a0ec8fe6daf25f281063cba597e7bb369dcb2e
Gianluca Barranca <info@gianlucabarranca.it>
2025-01-30 20:23:26 +0100

    Remove EntriesProvider wrapper from main page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { EntriesProvider } from "./context/EntriesContext";
 import Navbar from "./components/Navbar";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 
 export default function EntriesManager() {
   return (
-    <EntriesProvider>
-      <div className="flex flex-col min-h-screen bg-gray-100">
-        <Navbar />
-        <div className="flex-grow flex">
-          <MainContent />
-        </div>
-        <Footer />
+
+    <div className="flex flex-col min-h-screen bg-gray-100">
+      <Navbar />
+      <div className="flex-grow flex">
+        <MainContent />
       </div>
-    </EntriesProvider>
+      <Footer />
+    </div>
   );
 }
