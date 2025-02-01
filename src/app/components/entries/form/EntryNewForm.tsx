2149fddcc077e6bc03929298fa3a598541c1d6bb
Gianluca Barranca <info@gianlucabarranca.it>
2025-02-01 17:34:39 +0100

    add css class to button save & cancel

--- src/app/components/entries/form/EntryNewForm.tsx
+++ src/app/components/entries/form/EntryNewForm.tsx
@@ -116,12 +116,19 @@ export function EntryNewForm({
               label="Is Dangerous?"
               errors={errors}
             />
-            <TagInput name="tags" label="Tags" errors={errors} />
+            {/* <TagInput name="tags" label="Tags" errors={errors} />*/}
             <div className="flex justify-between mt-6">
-              <button type="button" onClick={onClose} className="btn-cancel">
+              <button
+                type="button"
+                onClick={onClose}
+                className="px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 Cancel
               </button>
-              <button type="submit" className="btn-save">
+              <button
+                type="submit"
+                className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              >
                 Save
               </button>
             </div>
